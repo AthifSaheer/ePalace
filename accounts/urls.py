e0024fd810cd5337aefaa7936c4fae10266a4205
Athif Saheer <liteboook@gmail.com>
2021-05-02 11:04:57 +0530

    admin and user auth created

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 urlpatterns = [
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
+    path('logout', views.logout, name='logout'),
+
+    path('admin-login', views.admin_login, name='admin_login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
 
 
 def login(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        auth_login(request, user)
+        return redirect('signup')
+    else:
+        uname_error = 'Invalid creditials ! !'
+        print(uname_error)
+        return render(request, 'User/login.html', {'uname_error':uname_error})
     return render(request, 'User/login.html')
 
 def signup(request):
@@ -14,7 +26,7 @@ def signup(request):
         confirm_password = request.POST.get('confirm-password')
         if password == confirm_password:
             user = User.objects.create_user(username=username, email=email, password=password)
-            # user.save()
+            auth_login(request, user)
             print("user created")
             return redirect('login')
         else:
@@ -22,3 +34,23 @@ def signup(request):
             print(pword_error)
             return render(request, 'User/signup.html', {'pword_error':pword_error})
     return render(request, 'User/signup.html')
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
+
+def admin_login(request):
+    uname = 'admin'
+    pword = 'admin'
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    if username == uname and password == pword:
+        print('admin logged in')
+        # return redirect('admin_home')
+    else:
+        invalid_error = "Invalid creditials ! !"
+        print(invalid_error)
+    return render(request, 'Admin/admin_login.html', {'invalid_error':invalid_error})
