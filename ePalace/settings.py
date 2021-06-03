d6d4cf893eb06319e12a63dab93101f3f7db328d
Athif Saheer <liteboook@gmail.com>
2021-06-03 11:53:36 +0530

    jun 3 2021 - 12:00

--- ePalace/settings.py
+++ ePalace/settings.py
@@ -1,5 +1,5 @@
 import os
-from ePalace.email import EMAIL, PASSWORD
+# from ePalace.email import EMAIL, PASSWORD
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
